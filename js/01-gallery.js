d0afbbcc227150d027892f10654f2219f411b18f
Adaptist <adaptist1@gmail.com>
2023-04-27 00:09:26 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,18 +7,18 @@ console.log(galleryItems);
 const gallery = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems.map(({preview, original, description}) => {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img 
-            class="gallery__image" 
-            src="${preview}" 
-            data-source="${original}" 
-            alt="${description}" 
-        />
-      </a>
-    </li>
-  `;
+    return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                data-source="${original}" 
+                alt="${description}" 
+            />
+        </a>
+        </li>
+    `;
 }).join('');
 
 gallery.insertAdjacentHTML('beforeend', galleryMarkup);
