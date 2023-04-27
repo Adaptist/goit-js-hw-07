619756e14c15e5089765e640ef5261a9f11d8301
Adaptist <adaptist1@gmail.com>
2023-04-27 15:50:38 +0300

    7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,58 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
-console.log(galleryItems);
+// console.log(galleryItems);
 
-// Разметка
+const galleryList = document.querySelector('.gallery');
 
-const gallery = document.querySelector('.gallery');
-
-const galleryMarkup = galleryItems.map(({preview, original, description}) => {
+// ================
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
     return `
-        <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img 
-                class="gallery__image" 
-                src="${preview}" 
-                data-source="${original}" 
-                alt="${description}" 
-            />
-        </a>
-        </li>
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image" 
+          src="${preview}" 
+          data-source="${original}" 
+          alt="${description}" 
+        />
+      </a>
+      </li>
     `;
-}).join('');
+  })
+  .join('');
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// Делегирование
+// ================
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
 
-gallery.addEventListener('click', event => {
-    event.preventDefault();
-  
-    if (event.target.nodeName !== 'IMG') {
-      return;
-    }
-  
-    const imgSrc = event.target.dataset.source;
-    const imgAlt = event.target.alt;
-  
-    openModal(imgSrc, imgAlt);
+  if (event.target.nodeName !== 'IMG') return;
+
+  const imgSrc = event.target.dataset.source;
+  const imgAlt = event.target.alt;
+
+  openModal(imgSrc, imgAlt);
+});
+
+// ================
+const openModal = (src, alt) => {
+  const modal = basicLightbox.create(`<img src="${src}" alt="${alt}" />`, {
+    onShow: () => {
+      document.addEventListener('keydown', onKeyDown);
+    },
+    onClose: () => {
+      document.removeEventListener('keydown', onKeyDown);
+    },
   });
-  
-  function openModal(src, alt) {
-    const modal = basicLightbox.create(
-      `<img src="${src}" alt="${alt}" />`
-    );
-  
-    modal.show();
-  }
+
+  const onKeyDown = event => {
+    if (event.code === 'Escape') {
+      modal.close();
+    }
+  };
+
+  modal.show();
+};
